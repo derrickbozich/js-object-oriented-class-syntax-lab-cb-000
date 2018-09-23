1c700954f391040aa8fc46d1e0d6f113c7889fe5
derrick b <dbozich1@gmail.com>
2018-09-23 02:48:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ class BoardMember {
     this.training = training;
   }
 
-  veto() => "No, I must disagree";
-  approve() => "You can do that!";
-  doCharity() => "I like to help people.";
-  releasePressStatement() => "You will see great things from Scuber.";
-  sayHi() => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  veto() {return "No, I must disagree"; }
+  approve() {return "You can do that!"; }
+  doCharity() {return "I like to help people."; }
+  releasePressStatement() {return "You will see great things from Scuber."; }
+  sayHi() {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`} 
 }
